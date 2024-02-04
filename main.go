6ededec69c94896e52b84c73e057beb1dc8d6910
yuz <yuzyuzx@gmail.com>
2024-02-04 19:10:52 +0900

    ver1 完成

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("aaa")
+	var isGameEnd = false
+	var inputNumber int
+	inputTimes := 0
+
+	rand.NewSource(time.Now().UnixNano())
+	answer := rand.Intn(100) + 1
+
+	for {
+		fmt.Println("１から１００までの数字を入力してください")
+		fmt.Scanln(&inputNumber)
+		inputTimes += 1
+		if inputNumber < 1 || 100 < inputNumber {
+			fmt.Println("無効な数字です。１から１００までの数字を入力してください")
+		}
+		fmt.Printf("debug: answer=%d, input=%d", answer, inputNumber)
+
+		for {
+
+			switch {
+			case answer == inputNumber:
+				fmt.Printf("正解です！！！%d回目で正解しました", inputTimes)
+				isGameEnd = true
+			case answer < inputNumber:
+				fmt.Println("もっと小さい数字です")
+			case inputNumber < answer:
+				fmt.Println("もっと大きい数字です")
+			default:
+				fmt.Println("無効な数字です。１から１００までの数字を入力してください")
+			}
+
+			if isGameEnd {
+				break
+			}
+
+			fmt.Scanln(&inputNumber)
+			inputTimes += 1
+		}
+
+		if isGameEnd {
+			break
+		}
+	}
 }
