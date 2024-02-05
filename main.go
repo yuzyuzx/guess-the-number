a20db28010cb26b234d65616bf88fe4857455100
yuz <yuzyuzx@gmail.com>
2024-02-05 17:44:49 +0900

    標準入力の受け取りをbufioに変更

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
   var isGameEnd = false
-  var inputNumber string
   inputTimes := 0
 
   rand.NewSource(time.Now().UnixNano())
@@ -17,35 +19,34 @@ func main() {
 
   fmt.Printf("debug: answer=%d\n", answer)
 
-  fmt.Println("数当てゲームです。正解は１から１００までの数字です。")
-  fmt.Println()
-  fmt.Println("１から１００までの数字を入力してください")
+  fmt.Println("数当てゲームをしましょう。正解は１から１００までの数字です。")
+  fmt.Println("数字を入力してください")
 
   for {
-    fmt.Scanf("%s", &inputNumber)
-    num, err := strconv.Atoi(inputNumber)
+    scanner := bufio.NewScanner(os.Stdin)
+
+    if(!scanner.Scan()) {
+      fmt.Println("読み込みに失敗しました")
+      continue
+    }
+
+    text := strings.TrimSpace(scanner.Text())
+    num, err := strconv.Atoi(text)
     if err != nil {
-      fmt.Println(err)
       fmt.Println("入力が正しくありません")
       continue
     }
 
-
-
-    if 0 <= num && num <= 100 {
-      inputTimes += 1
-    }
+    inputTimes += 1
 
     switch {
-    case answer == num:
-      fmt.Printf("正解です！！！%d回目で正解しました\n", inputTimes)
-      isGameEnd = true
     case answer < num:
       fmt.Println("もっと小さい数字です")
     case num < answer:
       fmt.Println("もっと大きい数字です")
     default:
-      fmt.Println("無効な数字です。１から１００までの数字を入力してください")
+      fmt.Printf("正解です！%d回目で正解しました\n", inputTimes)
+      isGameEnd = true
     }
 
     if isGameEnd {
