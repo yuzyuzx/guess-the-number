9c2193986832a586915693ee0fd6c669bfc611b9
yuz <yuzyuzx@gmail.com>
2024-02-05 14:42:36 +0900

    ループを１回に修正

--- main.go
+++ main.go
@@ -16,51 +16,35 @@ func main() {
 
   fmt.Printf("debug: answer=%d\n", answer)
 
+  fmt.Println("数当てゲームです。正解は１から１００までの数字です。")
+  fmt.Println()
+  fmt.Println("１から１００までの数字を入力してください")
+
   for {
-    fmt.Println("数当てゲームです。正解は１から１００までの数字です。")
-    fmt.Println("なるべく少ない回数で当ててみましょう。")
-    fmt.Println()
-    fmt.Println("１から１００までの数字を入力してください")
-    // fmt.Scanln(&inputNumber)
     _, err := fmt.Scanf("%d", &inputNumber)
     if err != nil {
-      //errors.New("不正な入力")
-      fmt.Printf("%s", "不正な入力")
+      fmt.Println("不正な入力です\n")
+      continue
     }
 
     if 0 <= inputNumber && inputNumber <= 100 {
       inputTimes += 1
     }
 
-    for {
+    switch {
+    case answer == inputNumber:
+      fmt.Printf("正解です！！！%d回目で正解しました\n", inputTimes)
+      isGameEnd = true
+    case answer < inputNumber:
+      fmt.Println("もっと小さい数字です")
+    case inputNumber < answer:
+      fmt.Println("もっと大きい数字です")
+    default:
+      fmt.Println("無効な数字です。１から１００までの数字を入力してください")
+    }
 
-      // fmt.Printf("%v", inputNumber)
-
-      switch {
-      case answer == inputNumber:
-        fmt.Printf("正解です！！！%d回目で正解しました", inputTimes)
-        isGameEnd = true
-      case answer < inputNumber:
-        fmt.Println("もっと小さい数字です")
-      case inputNumber < answer:
-        fmt.Println("もっと大きい数字です")
-      default:
-        fmt.Println("無効な数字です。１から１００までの数字を入力してください")
-      }
-
-      if isGameEnd {
-        break
-      }
-
-      // fmt.Scanln(&inputNumber)
-      _, err := fmt.Scanf("%d", &inputNumber)
-      if err != nil {
-        fmt.Printf("%s", "不正な入力")
-      }
-      if 0 <= inputNumber && inputNumber <= 100 {
-        inputTimes += 1
-      }
-      fmt.Println(inputNumber)
+    if isGameEnd {
+      break
     }
 
     if isGameEnd {
