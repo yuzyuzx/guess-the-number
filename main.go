bf004833f49eaae0db6d4b82e11e2e7bcd0f58dc
yuz <yuzyuzx@gmail.com>
2024-02-05 18:38:43 +0900

    for分にラベルを付けた

--- main.go
+++ main.go
@@ -1,56 +1,52 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "math/rand"
-  "os"
-  "strconv"
-  "strings"
-  "time"
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-  var isGameEnd = false
-  inputTimes := 0
+	inputTimes := 0
 
-  rand.NewSource(time.Now().UnixNano())
-  answer := rand.Intn(100) + 1
+	rand.NewSource(time.Now().UnixNano())
+	answer := rand.Intn(100) + 1
 
-  fmt.Printf("debug: answer=%d\n", answer)
+	fmt.Printf("debug: answer=%d\n", answer)
 
-  fmt.Println("数当てゲームをしましょう。正解は１から１００までの数字です。")
-  fmt.Println("数字を入力してください")
+	fmt.Println("数当てゲームをしましょう。正解は１から１００までの数字です。")
+	fmt.Println("数字を半角で入力してください")
 
-  for {
-    scanner := bufio.NewScanner(os.Stdin)
+	game:
+		for {
+			scanner := bufio.NewScanner(os.Stdin)
 
-    if !scanner.Scan() {
-      fmt.Println("読み込みに失敗しました")
-      continue
-    }
+			if !scanner.Scan() {
+				fmt.Println("読み込みに失敗しました")
+				continue
+			}
 
-    text := strings.TrimSpace(scanner.Text())
-    num, err := strconv.Atoi(text)
-    if err != nil {
-      fmt.Println("入力が正しくありません")
-      continue
-    }
+			text := strings.TrimSpace(scanner.Text())
+			num, err := strconv.Atoi(text)
+			if err != nil {
+				fmt.Println("入力が正しくありません")
+				continue
+			}
 
-    inputTimes += 1
+			inputTimes += 1
 
-    switch {
-    case answer < num:
-      fmt.Println("もっと小さい数字です")
-    case num < answer:
-      fmt.Println("もっと大きい数字です")
-    default:
-      fmt.Printf("正解です！%d回目で正解しました\n", inputTimes)
-      isGameEnd = true
-    }
-
-    if isGameEnd {
-      break
-    }
-  }
+			switch {
+			case answer < num:
+				fmt.Println("もっと小さい数字です")
+			case num < answer:
+				fmt.Println("もっと大きい数字です")
+			default:
+				fmt.Printf("正解です！%d回目で正解しました\n", inputTimes)
+				break game
+			}
+		}
 }
