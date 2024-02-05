2e2de96821ff6eda50d855d920c872b8bdde6a8c
yuz <yuzyuzx@gmail.com>
2024-02-05 14:37:37 +0900

    commit

--- main.go
+++ main.go
@@ -1,54 +1,70 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+  "fmt"
+  "math/rand"
+  "time"
 )
 
 func main() {
-	var isGameEnd = false
-	var inputNumber int
-	inputTimes := 0
+  var isGameEnd = false
+  var inputNumber int
+  inputTimes := 0
 
-	rand.NewSource(time.Now().UnixNano())
-	answer := rand.Intn(100) + 1
+  rand.NewSource(time.Now().UnixNano())
+  answer := rand.Intn(100) + 1
 
-	for {
-		fmt.Println("１から１００までの数字を入力してください")
-		fmt.Scanln(&inputNumber)
-		if 0 <= inputNumber && inputNumber <= 100 {
-      inputTimes += 1
-		} else {
-			fmt.Println("無効な数字です。１から１００までの数字を入力してください")
+  fmt.Printf("debug: answer=%d\n", answer)
+
+  for {
+    fmt.Println("数当てゲームです。正解は１から１００までの数字です。")
+    fmt.Println("なるべく少ない回数で当ててみましょう。")
+    fmt.Println()
+    fmt.Println("１から１００までの数字を入力してください")
+    // fmt.Scanln(&inputNumber)
+    _, err := fmt.Scanf("%d", &inputNumber)
+    if err != nil {
+      //errors.New("不正な入力")
+      fmt.Printf("%s", "不正な入力")
     }
 
-		fmt.Printf("debug: answer=%d, input=%d\n", answer, inputNumber)
+    if 0 <= inputNumber && inputNumber <= 100 {
+      inputTimes += 1
+    }
 
-		for {
+    for {
 
-			switch {
-			case answer == inputNumber:
-				fmt.Printf("正解です！！！%d回目で正解しました", inputTimes)
-				isGameEnd = true
-			case answer < inputNumber:
-				fmt.Println("もっと小さい数字です")
-			case inputNumber < answer:
-				fmt.Println("もっと大きい数字です")
-			default:
-				fmt.Println("無効な数字です。１から１００までの数字を入力してください")
-			}
+      // fmt.Printf("%v", inputNumber)
 
-			if isGameEnd {
-				break
-			}
+      switch {
+      case answer == inputNumber:
+        fmt.Printf("正解です！！！%d回目で正解しました", inputTimes)
+        isGameEnd = true
+      case answer < inputNumber:
+        fmt.Println("もっと小さい数字です")
+      case inputNumber < answer:
+        fmt.Println("もっと大きい数字です")
+      default:
+        fmt.Println("無効な数字です。１から１００までの数字を入力してください")
+      }
 
-			fmt.Scanln(&inputNumber)
-			inputTimes += 1
-		}
+      if isGameEnd {
+        break
+      }
 
-		if isGameEnd {
-			break
-		}
-	}
+      // fmt.Scanln(&inputNumber)
+      _, err := fmt.Scanf("%d", &inputNumber)
+      if err != nil {
+        fmt.Printf("%s", "不正な入力")
+      }
+      if 0 <= inputNumber && inputNumber <= 100 {
+        inputTimes += 1
+      }
+      fmt.Println(inputNumber)
+    }
+
+    if isGameEnd {
+      break
+    }
+  }
 }
