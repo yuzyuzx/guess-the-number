50b6d1fcb4abde22e2525c715ea9711df526edc6
yuz <yuzyuzx@gmail.com>
2024-02-06 16:16:38 +0900

    メッセージをMAP型にして関数化

--- main.go
+++ main.go
@@ -12,16 +12,16 @@ import (
 )
 
 type AnswerResult struct {
-  NumberResult numberResult
-  Message string
+	NumberResult answerResultType
+	Message      string
 }
 
-type numberResult int
+type answerResultType int
 
 const (
-  isLess numberResult= 1
-  isGreater numberResult = 2
-  isCorrect numberResult = 3
+	isLess    answerResultType = 1
+	isGreater answerResultType = 2
+	isCorrect answerResultType = 3
 )
 
 func main() {
@@ -37,24 +37,41 @@ func main() {
 			continue
 		}
 
-    answerCount += 1
+		answerCount += 1
 
 		result := checkAnswer(answer, userAnswer, answerCount)
-    fmt.Println(result.Message)
-    if result.NumberResult == 3 {
-      answerCount = 0
-      answer = generateAnswer()
-      break
-    }
+		fmt.Println(result.Message)
+		if result.NumberResult == isCorrect {
+			answerCount = 0
+			answer = generateAnswer()
+			break
+		}
 	}
+}
+
+func promptMessage() map[string]string {
+	msg := map[string]string{}
+
+	msg["1001"] = "数当てゲームをしましょう。"
+	msg["1002"] = "正解は１から１００までのランダムな数字です。"
+	msg["1003"] = "数字（半角）を入力してください。"
+
+	msg["2001"] = "残念！もっと小さい数字です。もう一度トライしてみてください。"
+	msg["2002"] = "残念！もっと大きい数字です。もう一度トライしてみてください。"
+	msg["2003"] = "おめでとうございます！"
+
+	msg["9001"] = "読み込みに失敗しました。もう一度お試しください。"
+	msg["9002"] = "入力が正しくありません。数字を入力してください。"
+
+	return msg
 }
 
 /**
 * ゲーム開始のメッセージ
  */
 func printGameStartPrompt() {
-	fmt.Println("数当てゲームをしましょう。正解は１から１００までのランダムな数字です。")
-	fmt.Println("数字（半角）を入力してください。")
+	msg := promptMessage()
+	fmt.Printf("%s\n%s\n\n%s\n", msg["1001"], msg["1002"], msg["1003"])
 }
 
 /*
@@ -73,15 +90,16 @@ func generateAnswer() int {
 標準入力からユーザーが入力した値を読み込む
 */
 func readUserAnswer() (int, error) {
+	msg := promptMessage()
 	scanner := bufio.NewScanner(os.Stdin)
 	if !scanner.Scan() {
-		return -1, errors.New("読み込みに失敗しました。もう一度お試しください。")
+		return -1, errors.New(msg["9001"])
 	}
 
 	text := strings.TrimSpace(scanner.Text())
 	num, err := strconv.Atoi(text)
 	if err != nil {
-		return -1, errors.New("入力が正しくありません。数字を入力してください。")
+		return -1, errors.New(msg["9002"])
 	}
 
 	return num, nil
@@ -91,24 +109,26 @@ func readUserAnswer() (int, error) {
 * 答え合わせ
  */
 func checkAnswer(answer, userAnswer, answerCount int) AnswerResult {
-  result := AnswerResult{}
+	msg := promptMessage()
+	result := AnswerResult{}
 
 	switch {
 	case answer < userAnswer:
-    result.NumberResult = isLess;
-    result.Message = "残念！もっと小さい数字です。もう一度トライしてみてください。"
+		result.NumberResult = isLess
+		result.Message = msg["2001"]
 	case userAnswer < answer:
-    result.NumberResult = isGreater;
-    result.Message = "残念！もっと大きい数字です。もう一度トライしてみてください。"
+		result.NumberResult = isGreater
+		result.Message = msg["2002"]
 	default:
-    result.NumberResult= isCorrect;
-    result.Message = fmt.Sprintf(
-      "おめでとうございます！%d回目で正解しました。\n",
-      answerCount,
-    )
+		result.NumberResult = isCorrect
+		result.Message = fmt.Sprintf(
+			"%s%d回目で正解しました。\n",
+			msg["2003"],
+			answerCount,
+		)
 	}
 
-  return result
+	return result
 }
 
 /**
@@ -117,20 +137,20 @@ func checkAnswer(answer, userAnswer, answerCount int) AnswerResult {
 func isRestart() bool {
 	fmt.Println("もう一度ゲームをしますか？")
 	fmt.Println("y:もう一度する, n:終わる")
-  scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	if !scanner.Scan() {
 	}
 
 	text := strings.TrimSpace(scanner.Text())
-  fmt.Println(text)
+	fmt.Println(text)
 
-  if(text == "y") {
-    fmt.Println("もう一度開始")
-    return true
-  } 
+	if text == "y" {
+		fmt.Println("もう一度開始")
+		return true
+	}
 
-  fmt.Println("終了します")
-  return false
+	fmt.Println("終了します")
+	return false
 }
 
 func restart() {
